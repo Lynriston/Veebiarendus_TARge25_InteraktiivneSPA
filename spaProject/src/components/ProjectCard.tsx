f1fc8d7c45abc28a40b9b3748d43fefa4d9261e7
Lynriston <xuliosofficial@gmail.com>
2026-04-19 13:44:16 +0300

    Update ProjectCard.tsx

--- spaProject/src/components/ProjectCard.tsx
+++ spaProject/src/components/ProjectCard.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
+// Kuvab projekti kaardi, mis sisaldab projekti pealkirja, 
+// kirjeldust ning nuppe meeldimiseks, 
+// kustutamiseks ja pealkirja muutmiseks. 
+// Pealkirja klikkides saab selle muuta.
 export default function ProjectCard(props: any) {
-    const { project, deleteProject, toggleLike, editProject } = props;
-    if (!project) return null;
-    const [isEditing, setIsEditing] = useState(false);
-    const [newTitle, setNewTitle] = useState(project?.title || "");
+    const { project, deleteProject, toggleLike, editProject } = props; //Võtab propsidest projekti andmed ja funktsioonid, mida kasutatakse projekti haldamiseks
+    if (!project) return null; // Kui projekti pole, ei renderdata midagi
+    const [isEditing, setIsEditing] = useState(false); //Kas pealkiri on muutmise režiimis
+    const [newTitle, setNewTitle] = useState(project?.title || ""); //Hoiab inputi väärtust pealkirja muutmisel
 
 
     return (
         <div className="card">
             {isEditing ? (
+                //kontrollib kas pealkiri on muutmise režiimis. Kui on, kuvab inputi, kus saab pealkirja muuta. Kui input kaotab fookuse (onBlur), salvestatakse uus pealkiri ja väljutakse muutmise režiimist.
                 <input
                     value={newTitle}
                     onChange={e => setNewTitle(e.target.value)}
